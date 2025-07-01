b88c23fec55224b532a133e2d5f8960f1dc89ef5
Pablo Andrés Díaz <pabloandresd@gmail.com>
2025-07-01 15:32:31 -0500

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	loadEnvVars()
 	routes := router.SetupRouter(actorModel.CreateNewAlbumManager())
-	router.RunHttpServerWithRoutes("localhost", "8080", routes)
+	router.RunHttpServerWithRoutes(getEnvVar("ON_HOST", "localhost"), getEnvVar("ON_PORT", "8080"), routes)
 }
 
 func loadEnvVars() {
@@ -20,4 +20,13 @@ func loadEnvVars() {
 	if err != nil && !os.IsNotExist(err) {
 		panic(fmt.Sprintf("Error loading .env files: %v", err))
 	}
+}
+
+func getEnvVar(withName string, defaultValueWhenNotPresent string) string {
+	maybeValueFoundForEnvVar, exists := os.LookupEnv(withName)
+	if !exists {
+		return defaultValueWhenNotPresent
+	}
+
+	return maybeValueFoundForEnvVar
 }
